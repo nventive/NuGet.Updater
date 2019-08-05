fa3ad82145eee4fb920d963d9bbb9bc8b66532ba
Benjamin Cartier <benjamin.cartier@nventive.com>
2019-08-05 12:01:53 -0400

    Update

--- src/NuGet.Updater/Helpers/FileHelper.UAP.cs
+++ src/NuGet.Updater/Helpers/FileHelper.UAP.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Storage;
@@ -10,10 +9,40 @@ using Windows.Storage.Search;
 
 namespace NuGet.Updater.Helpers
 {
+	/// <summary>
+	/// UAP-Specific methods.
+	/// </summary>
 	partial class FileHelper
 	{
 		public static void LogToFile(string outputFilePath, IEnumerable<string> log)
 		{
+		}
+
+		public static async Task<string[]> GetFiles(
+			CancellationToken ct,
+			string path,
+			string extensionFilter = null, 
+			string nameFilter = null
+		)
+		{
+			var folder = await StorageFolder.GetFolderFromPathAsync(path);
+
+			var searchFilter = extensionFilter == null
+				? $"filename:\"{nameFilter}\""
+				: $"extension:{extensionFilter}";
+
+			var queryOptions = new QueryOptions
+			{
+				UserSearchFilter = searchFilter,
+				IndexerOption = IndexerOption.UseIndexerWhenAvailable,
+				FolderDepth = FolderDepth.Deep,
+			};
+
+			var query = folder.CreateFileQueryWithOptions(queryOptions);
+
+			var files = await query.GetFilesAsync();
+
+			return files.Select(f => f.Path).ToArray();
 		}
 
 		public static async Task<string> ReadFileContent(CancellationToken ct, string path)
@@ -27,8 +56,22 @@ namespace NuGet.Updater.Helpers
 
 		public static async Task SetFileContent(CancellationToken ct, string path, string content)
 		{
-			var file = await StorageFile.GetFileFromPathAsync(path);
+			var file = await StorageFile.GetFileFromPathAsync(path).AsTask(ct);
 			await FileIO.WriteTextAsync(file, content);
+		}
+
+		public static async Task<bool> IsDirectory(CancellationToken ct, string path)
+		{
+			try
+			{
+				var folder = await StorageFolder.GetFolderFromPathAsync(path).AsTask(ct);
+
+				return true;
+			}
+			catch(ArgumentException)
+			{
+				return false;
+			}
 		}
 	}
 }
