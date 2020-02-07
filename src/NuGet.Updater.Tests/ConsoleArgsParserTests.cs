213db472a59b72f7d00b8470b6fd0b6141b3ba0a
Xiaoy312 <xiaoyao312@gmail.com>
2020-02-07 16:44:15 -0500

    Added ConsoleArgsParserTests

---
+++ src/NuGet.Updater.Tests/ConsoleArgsParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Updater.Tool;
+
+namespace NuGet.Updater.Tests
+{
+	[TestClass]
+	public class ConsoleArgsParserTests
+	{
+		[TestMethod]
+		public void Given_HelpArgument_ContextIsHelp()
+		{
+			var arguments = new[] { "-help" };
+			var (context, _) = ConsoleArgsParser.Parse(arguments);
+
+			Assert.IsTrue(context.IsHelp);
+		}
+
+		[TestMethod]
+		public void Given_InvalidArgument_ContextIsError()
+		{
+			var arguments = new[] { "--absolutelyWrong" };
+			var (context, _) = ConsoleArgsParser.Parse(arguments);
+
+			//throw new NotImplementedException();
+			Assert.IsTrue(context.IsHelp);
+		}
+	}
+}
